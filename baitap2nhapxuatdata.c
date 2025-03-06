5965dc8d5bfc13a76ea4b329f468f2af19290971
namhoai <namhoai3738@gmail.com>
2025-03-06 16:58:55 +0700

    Add files via upload

---
+++ baitap2nhapxuatdata.c
@@ -0,0 +1,15 @@
+      #include<stdio.h>
+      #include<math.h>
+      int main()
+      {
+        int a,b,c;
+        printf("MOI NGUOI DEP NHAP VAO NGAY  :  ");
+        scanf("%d",&a);
+        printf("MOI NGUOI DEP NHAP VAO THANG :  ");
+        scanf("%d",&b);
+        printf("MOI NGUOI DEP NHAP VAO NAM   :  ");
+        scanf("%d",&c);
+        printf("HOM NAY LA :/%d /%d /%d",a,b,c);
+
+         return 0;
+      }
