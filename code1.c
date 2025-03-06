d86fb0300a72767eeb04bf7a1d2a78b1ea6f62f9
namhoai <namhoai3738@gmail.com>
2025-03-06 16:21:44 +0700

    Add files via upload
     Commit directly to the main branch.
     Create a new branch for this commit and start a pull request. Learn more about pull requests.

---
+++ code1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int  a,b,c,d;
+printf("ban hay nhap vao so a :");
+scanf("%d",&a);
+
+b=a*a;
+c=a*a*a;
+d=a*a*a*a;
+printf("day la ket qua cua ban : %d %d %d ",b,c,d);
+}
