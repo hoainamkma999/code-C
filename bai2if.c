a345a866a9c0be243e8277de1d48ada41bfd3858
namhoai <namhoai3738@gmail.com>
2025-03-11 21:08:29 +0700

    Add files via upload

---
+++ bai2if.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a ,b,c,d,e,f,x,y;
+printf("Moi nguoi dep nhap so :\n");
+scanf("%f %f %f %f %f %f",&a,&b,&c,&d,&e,&f);
+y=(f-((d*c)/a))/(e-((d*b)/a));
+x=(c-(b*y))/a;
+printf("Gia tri cua nguoi dep la :\n");
+printf("x = %.2f \n",x);
+printf("y = %.2f", y);
+return 0;
+
+
+}
